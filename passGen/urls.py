b76c86b0ced737aa0fe22c8352308eac83e894cb
MinhHoang <minhhoangdayyyy07@gmail.com>
2025-04-10 21:44:20 +0700

    update new URL view for server

--- passGen/urls.py
+++ passGen/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from passWordGenFunction import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home),
+    path('passwordGen/',views.passGen)
 ]
